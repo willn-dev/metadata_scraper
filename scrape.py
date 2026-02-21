6e40e287f6070af0a66b4b5292f1c904e5988f87
Will N <146034825+HelpItsAllGooberedUp@users.noreply.github.com>
2026-02-20 22:26:07 -0600

    got parsed data from jpeg, sort not yet implemented

---
+++ scrape.py
@@ -0,0 +1,23 @@
+
+raw_data = []
+parsed = []
+
+with open('assets/juniper_butterfly.jpeg', 'rb') as f:
+    byte_obj = f.read()
+
+    length = len(byte_obj)
+
+    for i, v in enumerate(byte_obj):
+
+        if v == 255 and i < length -1: 
+
+            raw_data.append(byte_obj[i+1])
+
+
+
+for each in raw_data:
+    if each != 0:
+        parsed.append(each)
+
+
+print(parsed)
