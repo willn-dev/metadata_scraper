7572d5f3a0403930c61195261402b9d0255868d0
Will N <146034825+HelpItsAllGooberedUp@users.noreply.github.com>
2026-02-26 22:49:27 -0600

    implemented an extraction into individual exif ifd entries.

---
+++ exif_inf.py
@@ -0,0 +1,28 @@
+exif_tag = {
+
+    0x010d: 'Document Name:',
+    0x010e: 'Image Description',
+    0x0131: 'Software :',
+    0x0132: 'Modify Date:',
+    0x013b: 'Artist:',
+    0x013c: 'Host Computer:',
+    0x8825:'GPS INFO POINTER',
+    0x9003: 'Date/Time Original:',
+    0x9213: 'Revision History:',
+    0xa430: 'Device Owner Name:',
+    0xa431: 'Camera Serial Num:',
+    0xa432: 'Lens Info:',
+    0xa433: 'Lens Make:',
+    0xa434: 'Lens Model:',
+    0xa435: 'Lens Serial Num:',
+    0xa436: 'Image Title:',
+    0xa437: 'Photographer',
+    0xa438: 'Editor:',
+    0x838f: 'Battery Level:',
+    # Camera settings/hardware.
+    0x829a: 'Exposure Time:',
+    0x82a6: 'MD Scale Pixel:',
+    0x8649: 'Photoshop Settings',
+    0x8827: 'ISO'
+
+}
--- scrape.py
+++ scrape.py
@@ -1,24 +1,73 @@
+from exif_inf import exif_tag
 
-raw_data = []
-parsed = []
 
-with open('assets/juniper_butterfly.jpeg', 'rb') as f:
-    byte_obj = f.read()
+# ----> 225 is flag for EXIF START <----###
+def main():
+    raw_data = [] #full data
+    parsed = [] # list of indices containing exif values.
+    byte_order = ''# is set to big or little endian
 
-    length = len(byte_obj)
 
-    for i, v in enumerate(byte_obj):
+    with open('assets/juniper_butterfly.jpeg', 'rb') as f:
+        byte_obj = f.read()
 
-        if v == 255 and i < length -1: 
+        length = len(byte_obj)
 
-            raw_data.append((i, byte_obj[i + 1]))
+        for i, v in enumerate(byte_obj):
+
+            if v == 255 and i < length -1: 
+
+                raw_data.append((i, byte_obj[i + 1]))
             
 
 
 
-for each in raw_data:
-    if each[1] != 0:
-        parsed.append(each)
+    for each in raw_data:
+        if each[1] != 0:
+            parsed.append(each)
 
 
-print(parsed)
+    for each in parsed:
+        if each[1] == 225:
+            exif_start = each[0]
+
+            #BIG OR LITTLE ENDIAN DETERMINATION
+            b_order = byte_obj[exif_start + 10: exif_start +12] # position of MM/II 2byte order marker.
+
+            if b_order == b'MM':
+                byte_order = 'big'
+            elif b_order == b'II':
+                byte_order = 'little'
+
+            head_len = byte_obj[exif_start + 2: exif_start + 4] # Exif header 6 byte total. skipping first values and just reading len
+
+            exif_len = int.from_bytes(head_len, byteorder=byte_order)
+            exif_end = exif_start + exif_len
+
+
+    byte_stepper(exif_start, exif_end, byte_obj, byte_order)
+#temp checks
+#print(byte_obj[exif_start:exif_end])
+#print("\n")
+#print(exif_len)
+#print(byte_order)
+
+
+
+def byte_stepper(start, end, byte_obj, byte_order):
+
+    ifd_entry_amt = int.from_bytes((byte_obj[start + 18 : start + 20]), byteorder= byte_order)
+    all_ifd_entries = byte_obj[start + 20: start + 20 + (ifd_entry_amt * 12)] # each ifd entry is 12 bytes long hence mult
+
+    # gets each ifd entry individually
+    #RESUME HERE NEED TO COMPARE WITH TABLE SEE MIDORI BYTE STEPPER ENTRY
+
+    for i in range(0, len(all_ifd_entries), 12):
+        single_ifd = all_ifd_entries[i:i + 12]
+        print(single_ifd)
+        print("\n")
+
+    #CYCLE THRU ENTRY AND COMPARE TO IFD STRUCTURE AND GO 2, 2, 4, 4,
+
+if __name__ == ("__main__"):
+    main()
